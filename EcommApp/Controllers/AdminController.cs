1170b1d8496610a8434ea8e741d80740e4d2130c
SannelliRegala <eu.laraga@gmail.com>
2022-03-02 11:41:35 +0800

    Made Admin view

--- EcommApp/Controllers/AdminController.cs
+++ EcommApp/Controllers/AdminController.cs
@@ -9,7 +9,7 @@ namespace EcommApp.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult Index()
+        public ActionResult Admin()
         {
             return View();
         }
